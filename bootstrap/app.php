e2fda94e150fc68c69c843025caf7b4f8d7e24ac
ImKevinL <libreros.kevin@correounivalle.edu.co>
2026-06-01 22:17:32 -0500

    trust proxies https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function ($middleware) {
-    $middleware->alias([
-        'role' => \App\Http\Middleware\RoleMiddleware::class,
-    ]);
-})
+        $middleware->trustProxies(at: '*');
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
+    })
 
     ->withExceptions(function (Exceptions $exceptions) {
         //
