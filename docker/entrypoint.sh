7c9bc960295cd345b2de5841c454f01da5e758df
ImKevinL <libreros.kevin@correounivalle.edu.co>
2026-06-02 14:40:15 -0500

    fix: entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,29 +1,9 @@
 #!/bin/sh
 set -e
 
-# Escribir variables de entorno de Render al .env del contenedor
-php -r "
-\$env = file_get_contents('/var/www/html/.env');
-\$vars = ['APP_KEY', 'APP_URL', 'APP_ENV', 'APP_DEBUG',
-          'DB_CONNECTION', 'DB_HOST', 'DB_PORT', 'DB_DATABASE', 'DB_USERNAME', 'DB_PASSWORD',
-          'CLOUDINARY_URL', 'CLOUDINARY_CLOUD_NAME', 'CLOUDINARY_API_KEY', 'CLOUDINARY_API_SECRET',
-          'SESSION_DRIVER', 'CACHE_STORE', 'QUEUE_CONNECTION', 'LOG_CHANNEL', 'LOG_LEVEL'];
-foreach (\$vars as \$var) {
-    \$val = getenv(\$var);
-    if (\$val !== false) {
-        \$env = preg_replace('/^' . \$var . '=.*/m', \$var . '=' . \$val, \$env);
-    }
-}
-file_put_contents('/var/www/html/.env', \$env);
-"
-
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
-
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 php artisan migrate --force
 php artisan storage:link || true
 
